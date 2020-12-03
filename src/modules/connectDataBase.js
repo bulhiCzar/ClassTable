67943bdbf7ef799cf1b30131ff49f875e00f7bab
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-12-03 23:22:36 +0300

    edit

--- src/modules/connectDataBase.js
+++ src/modules/connectDataBase.js
@@ -13,10 +13,10 @@ const DB = async () => {
         const connected = await mongoose.connect(GS.MDB.oneLink, connectInfo)
     } else {
         console.log('run product')
-        connectInfo.dbName = 'production'
-        connectInfo.user = GS.MDB.user
-        connectInfo.pass = GS.MDB.pass
-        const connected = await mongoose.connect(GS.MDB.link, connectInfo)
+        // connectInfo.dbName = 'production'
+        // connectInfo.user = GS.MDB.user
+        // connectInfo.pass = GS.MDB.pass
+        const connected = await mongoose.connect(`mongodb+srv://${GS.MDB.user}:${GS.MDB.pass}@testing.eb8wd.mongodb.net/production`, connectInfo)
     }
 }
 
