e14d0a418d7f738c6156a540444646de9d117b5c
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-12-23 21:21:27 +0300

    Update connectDataBase.js

--- src/modules/connectDataBase.js
+++ src/modules/connectDataBase.js
@@ -11,6 +11,7 @@ const DB = async () => {
     if (GS.MDB.ifOneLink && !process.env.NODE_ENV) {
         const connected = await mongoose.connect(GS.MDB.oneLink, connectInfo)
     } else {
+        console.log('connect prod BD')
         const connected = await mongoose.connect(`mongodb+srv://${GS.MDB.user}:${GS.MDB.pass}@testing.eb8wd.mongodb.net/production`, connectInfo)
     }
 }
