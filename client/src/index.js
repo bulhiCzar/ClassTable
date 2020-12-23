fa2586867d0d71ec7f7fba4a0a57aa9ebb4d11dc
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-12-23 21:40:23 +0300

    new build

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,22 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import thunk from "redux-thunk";
-import {applyMiddleware, compose, createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import {Provider} from "react-redux"
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 import reportWebVitals from './reportWebVitals';
 import {rootReducer} from "./redux/rootReducer";
 import App from './App';
 import './index.css';
 
 
-const store = createStore(rootReducer, compose(
-    applyMiddleware(
-        thunk
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
 
-
-
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk),
+    )
+)
 
 
 ReactDOM.render(
--- src/modules/connectDataBase.js
+++ src/modules/connectDataBase.js
@@ -8,12 +8,12 @@ const DB = async () => {
         useUnifiedTopology: true,
     }
 
-    if (GS.MDB.ifOneLink && !process.env.NODE_ENV) {
-        const connected = await mongoose.connect(GS.MDB.oneLink, connectInfo)
-    } else {
+    // if (GS.MDB.ifOneLink && !process.env.NODE_ENV) {
+    //     const connected = await mongoose.connect(GS.MDB.oneLink, connectInfo)
+    // } else {
         console.log('connect prod BD')
         const connected = await mongoose.connect(`mongodb+srv://${GS.MDB.user}:${GS.MDB.pass}@testing.eb8wd.mongodb.net/production`, connectInfo)
-    }
+    // }
 }
 
 module.exports = DB
